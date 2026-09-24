86756a06f6d07450381aa5e75843954502c7b201
Ville Oikkonen <wilmo@Villes-Mac-studio.local>
2026-09-24 12:07:54 +0300

    Add not found handling

--- app.py
+++ app.py
@@ -17,6 +17,8 @@ def index():
 @app.route("/recipe/<int:recipe_id>")
 def show_recipe(recipe_id):
     recipe = recipes.get_recipe(recipe_id)
+    if not recipe:
+        abort(404)
     return render_template("show_recipe.html", recipe=recipe)
 
 # User registeration
@@ -88,6 +90,8 @@ def new_recipe():
 @app.route("/edit_recipe/<int:recipe_id>")
 def edit_recipe(recipe_id):
     recipe = recipes.get_recipe(recipe_id)
+    if not recipe:
+        abort(404)
     if recipe["user_id"] != session["user_id"]:
         abort(403)
     return render_template("edit_recipe.html", recipe=recipe)
@@ -96,6 +100,8 @@ def edit_recipe(recipe_id):
 def update_recipe():
     recipe_id = request.form["recipe_id"]
     recipe = recipes.get_recipe(recipe_id)
+    if not recipe:
+        abort(404)
     if recipe["user_id"] != session["user_id"]:
         abort(403)
     title = request.form["title"]
@@ -108,6 +114,8 @@ def update_recipe():
 @app.route("/remove_recipe/<int:recipe_id>", methods=["GET", "POST"])
 def remove_recipe(recipe_id):
     recipe = recipes.get_recipe(recipe_id)
+    if not recipe:
+        abort(404)
     if recipe["user_id"] != session["user_id"]:
         abort(403)
     if request.method == "GET":
--- recipes.py
+++ recipes.py
@@ -18,7 +18,8 @@ def get_recipe(recipe_id):
              FROM recipes, users
              WHERE recipes.user_id = users.id AND
                    recipes.id = ?"""
-    return db.query(sql, [recipe_id])[0]
+    result = db.query(sql, [recipe_id])
+    return result[0] if result else None
 
 def update_recipe(recipe_id, title, description, ingredients):
     sql = """UPDATE recipes SET title = ?,
